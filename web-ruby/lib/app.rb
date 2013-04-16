532117b63876021934e253a7acc6fa8f97a470ed
Rodrigo Saito <rodrigo.saito@gmail.com>
2013-04-16 19:38:33 +0200

    Using poncho to build the json api

--- web-ruby/lib/app.rb
+++ web-ruby/lib/app.rb
@@ -3,11 +3,12 @@ require "java"
 require "sinatra/base"
 require "sinatra/json"
 require "sinatra/reloader"
+require "poncho"
 
 require_relative "spring/context"
 require_relative "sinatra/spring"
 
-
+# Populating database
 txManager = ::Spring::Context.bean_factory.get_bean("transactionManager")
 
 transactionTemplate = Java::OrgSpringframeworkTransactionSupport::TransactionTemplate.new txManager
@@ -28,6 +29,64 @@ end
 
 transactionTemplate.execute(AddCustomer.new)
 
+class CustomerResource < Poncho::Resource
+
+  param :id
+  param :name
+  param :email
+
+end
+
+class RetrieveCustomerListMethod < Poncho::JSONMethod
+  include Sinatra::Spring
+
+  param :id
+
+  def invoke
+    [].tap do |list|
+      spring_bean("customerRepository").all.each do |cust|
+        list << CustomerResource.new(cust)
+      end
+    end
+  end
+end
+
+class RetrieveCustomerMethod < Poncho::JSONMethod
+  include Sinatra::Spring
+
+  param :id, type: :integer, required: true
+
+  def invoke
+    CustomerResource.new(spring_bean("customerRepository").find(param(:id)))
+  end
+
+end
+
+class CreateCustomerMethod < Poncho::JSONMethod
+  include Sinatra::Spring
+
+  param :name
+  param :email
+
+  def invoke
+    puts "name = #{param(:name)}"
+    puts "email = #{param(:email)}"
+
+=begin
+    transactionTemplate.execute Class.new(Java::OrgSpringframeworkTransactionSupport::TransactionCallbackWithoutResult) do
+      def doInTransactionWithoutResult(status)
+        customer = Java::JavasinatraCoreModel::Customer.new
+        customer.name = param(:name)
+        customer.email = param(:email)
+
+        spring_bean("customerRepository").save(customer)
+      end
+    end.new
+=end
+  end
+
+end
+
 class JavaSinatra < Sinatra::Base
 
   configure :development do
@@ -37,11 +96,12 @@ class JavaSinatra < Sinatra::Base
   end
 
   helpers Sinatra::Spring
-  helpers Sinatra::JSON
 
-  get "/customers" do
-    spring_bean("customerRepository").all.to_s
-  end
+  post "/customers", &CreateCustomerMethod
+
+  get "/customers", &RetrieveCustomerListMethod
+
+  get "/customers/:id", &RetrieveCustomerMethod
 
 end
 
